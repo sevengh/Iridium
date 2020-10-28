ba1ffefac2f4e04819e96680c813376ad0c8c8a2
Sergey Shamanayev <shamanayev.sergey@gmail.com>
2020-10-28 20:52:31 +0400

    Implement audio control via keyboard

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AudioSwitcher.AudioApi.CoreAudio;
 
 namespace Iridium
 {
@@ -20,9 +21,31 @@ namespace Iridium
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly CoreAudioDevice defaultPlaybackDevice = null;
+
         public MainWindow()
         {
             InitializeComponent();
+            defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+        }
+
+        bool isActiveSoundKeys = true;
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F12)
+            {
+                isActiveSoundKeys = !isActiveSoundKeys;
+            }
+
+            else if (e.Key == Key.VolumeMute && isActiveSoundKeys)
+                defaultPlaybackDevice.SetMuteAsync(!defaultPlaybackDevice.IsMuted);
+
+            else if (e.Key == Key.VolumeUp && isActiveSoundKeys)
+                defaultPlaybackDevice.SetVolumeAsync(defaultPlaybackDevice.Volume + 1);
+
+            else if (e.Key == Key.VolumeDown && isActiveSoundKeys)
+                defaultPlaybackDevice.SetVolumeAsync(defaultPlaybackDevice.Volume - 1);
         }
     }
 }
