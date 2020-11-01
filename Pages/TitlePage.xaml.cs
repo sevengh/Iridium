622128b69197ed67db02791bbae21c7e180ccd58
Sergey Shamanayev <shamanayev.sergey@gmail.com>
2020-11-01 15:52:05 +0400

    Change windows name, add icon, add title page

---
+++ Pages/TitlePage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Iridium.Pages
+{
+    /// <summary>
+    /// Interaction logic for TitlePage.xaml
+    /// </summary>
+    public partial class TitlePage : Page
+    {
+        public TitlePage()
+        {
+            InitializeComponent();
+            Application.Current.MainWindow.KeyDown += new KeyEventHandler(Page_KeyDown);
+        }
+
+        private void Page_KeyDown(object sender, KeyEventArgs e)
+        {
+            Application.Current.MainWindow.KeyDown -= new KeyEventHandler(Page_KeyDown);
+            NavigateBrowser();
+        }
+
+        private void Page_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            NavigateBrowser();
+        }
+
+        void NavigateBrowser()
+        {
+            NavigationService?.Navigate(new Uri("Pages/BrowserPage.xaml", UriKind.Relative));
+        }
+    }
+}
