0bbc45f9d03ade37adad2ee421e9ffb2d0e2b14f
Sergey Shamanayev <shamanayev.sergey@gmail.com>
2021-04-16 09:02:18 +0400

    Do not run twice

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,32 @@ namespace Iridium
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Application name
+        /// </summary>
+        const string appName = "Iridium";
+
+
+        /// <summary>
+        /// Mutex
+        /// </summary>
+        public static Mutex Mutex { get; set; } = null;
+
+
+        /// <summary>
+        /// On startup
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Mutex = new Mutex(true, appName, out bool createdNew);
+
+            if (!createdNew)
+            {
+                Current.Shutdown();
+                return;
+            }
+        }
+
     }
 }
